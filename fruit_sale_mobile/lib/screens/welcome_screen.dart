cdd15fd16d5df7fbaaab746d246933f540c40cc4
GuilhermePPCompassUol <guilherme.petry.pb@compasso.com.br>
2022-10-07 18:08:08 -0300

    Implementado códigos na Welcome Screen e refatoração de códigos.

--- fruit_sale_mobile/lib/screens/welcome_screen.dart
+++ fruit_sale_mobile/lib/screens/welcome_screen.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 
-class Welcome_Screen extends StatelessWidget {
-  const Welcome_Screen({super.key});
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          color: Colors.orange,
-          
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              color: const Color(0xFFFFA451),
+              child: Image.asset('assets/images/WelcomeScreen/Component1.png'),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text("Let's continue"),
+            )
+          ],
         ),
       ),
     );
