e00e4b61f16a51eac799faf6679541fbfa909bbb
GuilhermePPCompassUol <guilherme.petry.pb@compasso.com.br>
2022-10-08 09:51:26 -0300

    Adicionado container e text button na welcome_screen

--- fruit_sale_mobile/lib/screens/splash_screen.dart
+++ fruit_sale_mobile/lib/screens/splash_screen.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:fruit_sale_mobile/screens/welcome_screen.dart';
 
-class Splash_Screen extends StatefulWidget {
-  const Splash_Screen({super.key});
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
 
   @override
-  State<Splash_Screen> createState() => _Splash_ScreenState();
+  State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _Splash_ScreenState extends State<Splash_Screen> {
+class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
@@ -16,11 +16,9 @@ class _Splash_ScreenState extends State<Splash_Screen> {
   }
 
   navigateToWelcomeScreen() async {
-    await Future.delayed(Duration(milliseconds: 3000), () {});
-    Navigator.pushReplacement(
-      context, 
-      MaterialPageRoute(
-        builder: (context) => Welcome_Screen()));
+    await Future.delayed(const Duration(milliseconds: 3000), () {});
+    Navigator.pushReplacement(context,
+        MaterialPageRoute(builder: (context) => const WelcomeScreen()));
   }
 
   @override
