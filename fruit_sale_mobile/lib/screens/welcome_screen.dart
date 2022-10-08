0462fb807babbc606778f38af98fa62d55b0fc10
GuilhermePPCompassUol <guilherme.petry.pb@compasso.com.br>
2022-10-08 19:16:52 -0300

    Correções

--- fruit_sale_mobile/lib/screens/welcome_screen.dart
+++ fruit_sale_mobile/lib/screens/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//import 'package:google_fonts/google_fonts.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
@@ -8,21 +9,40 @@ class WelcomeScreen extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Container(
               width: double.infinity,
               color: const Color(0xFFFFA451),
               child: Image.asset('assets/images/Component1.png'),
             ),
-            Container(),
-            const Text("Get The Freshest Fruit Salad Combo"),
-            const Text(
-                "We deliver the best and freshest fruit salad in town. Order for a combo today!!!"),
+            Padding(
+              padding: const EdgeInsets.only(top: 40, left: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text("Get The Freshest Fruit Salad Combo",
+                      style: TextStyle(
+                          color: Color(0xFF27214D),
+                          fontWeight: FontWeight.bold)),
+                  SizedBox(height: 8),
+                  Text(
+                    "We deliver the best and freshest fruit salad in town. Order for a combo today!!!",
+                    style: TextStyle(
+                      color: Color(0xFF5D577E),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 80),
             ElevatedButton(
               onPressed: () {},
-              style: const ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll(Color(0xFFFFA451))),
+              style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(56),
+                  maximumSize: const Size.fromWidth(327),
+                  //backgroundColor: 
+                  //backgroundColor: MaterialStatePropertyAll(Color(0xFFFFA451)),
+                  ),
               child: const Text(
                 "Let's continue",
                 style: TextStyle(
